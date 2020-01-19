424e8bcb160debf1c7ad9f3f733e0b4fd7cbfa83
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-01-19 17:13:51 -0600

    Create phpBasics.php

---
+++ phpBasics.php
@@ -0,0 +1,24 @@
+<?PHP
+$yourName = "Tamera";
+
+$number1 =5;
+$number2=3;
+$total = $number1 + $number2;
+
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>PHP Basics</title>
+
+</head>
+<body>
+<?PHP
+  echo"<h1>$yourName</h1>";
+
+  echo"<p>$number1 + $number2 = $total</p>";
+?>
+<h2><?PHP echo $yourName; ?></h2>
+</body>
+</html>
