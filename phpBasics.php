62f6bb59a70ee4ac8798187b2ac79ca098c4e63d
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-01-23 21:43:30 -0600

    Update phpBasics.php

--- phpBasics.php
+++ phpBasics.php
@@ -5,6 +5,7 @@ $number1 =5;
 $number2=3;
 $total = $number1 + $number2;
 
+$languageAgain = array("PHP", "HTML", "Javascript")
 ?>
 <!doctype html>
 <html>
@@ -12,12 +13,19 @@ $total = $number1 + $number2;
   <meta charset="utf-8">
   <title>PHP Basics</title>
 
+<script>
+
+var languages = [PHP, HTML, Javascript];
+
+</script>
 </head>
 <body>
 <?PHP
   echo"<h1>$yourName</h1>";
 
   echo"<p>$number1 + $number2 = $total</p>";
+
+  echo "<h1>$languageAgain</h1>";
 ?>
 <h2><?PHP echo $yourName; ?></h2>
 </body>
