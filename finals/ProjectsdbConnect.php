075d0d93a9bec7e3157461e9c5c54dc0bfe5a43a
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-04-29 15:38:46 -0500

    Create ProjectsdbConnect.php

---
+++ finals/ProjectsdbConnect.php
@@ -0,0 +1,26 @@
+<?php
+//PHP PDO Connection File
+//This File is used to connect to a database.
+//Include this file into your application as needed
+
+$serverName='localhost';
+$username='root'; //username of database, not account
+$password=''; //password of database
+$databaseName='projects';//name of the database you will be accesing
+  //on heartland, the username & databasename are the same.
+  //so when uploading it to my heartland account, find the username for heartland
+  //and that's the same for the databaseName
+
+
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+    }
+
+ ?>
