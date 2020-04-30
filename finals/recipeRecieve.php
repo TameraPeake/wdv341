f3d3d03b58a06afa462dad46d0e659edb9c5ae5c
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-04-29 20:30:02 -0500

    Update recipeRecieve.php

--- finals/recipeRecieve.php
+++ finals/recipeRecieve.php
@@ -1,22 +1,14 @@
 <?php
-require 'ProjectsdbConnect.php';
+//require 'ProjectsdbConnect.php';
 
-$sql = "SELECT ";
-$sql .= "recipe_name, ";
-$sql .= "recipe_ingredientsName, ";
-$sql .= "recipe_instructions, ";
-$sql .= "recipe_servingSize, ";
-$sql .= "recipe_units, ";
-$sql .= "recipe_ingredientsNumber ";
-$sql .= "FROM rifesrecipes";
+$db = mysqli_connect('localhost','root','','projects');
+$sth = mysqli_query($db, 'SELECT recipe_name, recipe_ingredientsName, recipe_instructions, recipe_servingSize, recipe_units, recipe_ingredientsNumber FROM rifesrecipes');
+$rows = array();
+while($r = mysqli_fetch_assoc($sth)) {
+    $rows[] = $r;
+}
 
-$stmt = $conn->prepare($sql);
-
-$stmt->execute();
-$stmt->fetch();
-
-
-
+echo json_encode($rows);
 /*
 foreach($_POST as $key => $value)		//This will loop through each name-value in the $_POST array
 {
