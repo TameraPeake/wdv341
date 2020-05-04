28e2cb2396b8e22e65b4e5ce580a455a58e520d0
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-05-04 09:32:05 -0500

    Update recipeRecieve.php

--- finals/recipeRecieve.php
+++ finals/recipeRecieve.php
@@ -1,14 +1,60 @@
 <?php
-//require 'ProjectsdbConnect.php';
+require 'ProjectsdbConnect.php';
 
+  header("Content-Type: application/json; charset=UTF-8");
+
+try {
+	//$productObj = new stdClass();
+  $sql = "SELECT ";
+  $sql .= "recipeID, ";
+  $sql .= "recipe_name, ";
+  $sql .= "recipe_ingredientsName, ";
+  $sql .= "recipe_instructions, ";
+  $sql .= "recipe_servingSize, ";
+  $sql .= "recipe_units, ";
+  $sql .= "recipe_ingredientsNumber ";
+  $sql .= "FROM rifesrecipes";
+
+  $stmt = $conn->prepare($sql);
+
+  $stmt->execute();
+
+  while($row=$stmt->fetch(PDO::FETCH_ASSOC)){
+   	$productObj[] = $row['recipe_name'];
+  }
+  //echo $productObj;
+
+	$returnObj = json_encode($productObj);
+
+  echo $returnObj;
+
+
+}
+
+   catch(PDOException $e) {
+     echo $message;
+
+     $message = "There has been a problem. The system administrator has been contacted. Please try again later.";
+
+     error_log($e->getMessage());			//Delivers a developer defined error message to the PHP log file at c:\xampp/php\logs\php_error_log
+     error_log($e->getLine());
+     error_log(var_dump(debug_backtrace()));
+
+     //Clean up any variables or connections that have been left hanging by this error.
+
+     header('Location: files/505_error_response_page.php');	//sends control to a User friendly page
+   };
+
+
+/*
 $db = mysqli_connect('localhost','root','','projects');
 $sth = mysqli_query($db, 'SELECT recipe_name, recipe_ingredientsName, recipe_instructions, recipe_servingSize, recipe_units, recipe_ingredientsNumber FROM rifesrecipes');
 $rows = array();
 while($r = mysqli_fetch_assoc($sth)) {
-    $rows[] = $r;
+    $rows[]= $r;
 }
 
-echo json_encode($rows);
+echo json_encode($rows);*/
 /*
 foreach($_POST as $key => $value)		//This will loop through each name-value in the $_POST array
 {
