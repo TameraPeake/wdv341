6bf7f76750abe90ba4d4950c30f24cfb8bebc543
TameraPeake <59899275+TameraPeake@users.noreply.github.com>
2020-05-04 09:31:27 -0500

    Update ProjectsdbConnect.php

--- finals/ProjectsdbConnect.php
+++ finals/ProjectsdbConnect.php
@@ -16,7 +16,7 @@ try {
     $conn = new PDO("mysql:host=$serverName;dbname=$databaseName", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    //echo "Connected successfully";
     }
 catch(PDOException $e)
     {
